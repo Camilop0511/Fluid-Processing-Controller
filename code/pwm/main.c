99006c5977cd4c06bf4ca6e4f75f3a3930da38e7
MaskedCamilo <camilo0599@gmail.com>
2023-03-15 12:51:03 -0400

    v3.2

--- code/pwm/main.c
+++ code/pwm/main.c
@@ -6,32 +6,24 @@
 #include <util/delay.h>
 
 #define BAUDRATE 8			//57.6k bps at 8MHz
+#define water_p1 OCR1A
+
 
 void usart_init(void);
 void uart_tx(unsigned char);
 unsigned char uart_rx(void);
 int ascii_to_numeric(char input[]);
+void pwm_init(void);
 
 int output[4];
 
 int main(void)
 {
-	// Set PD5 (OC1A) as output pin
-	DDRD |= (1 << PD5);
-
-	// Set Timer/Counter1 in Fast PWM mode, with non-inverted PWM output on OC1A
-	TCCR1A |= (1 << COM1A1) | (1 << COM1A0) | (1 << WGM11) | (1 << WGM10);		//COM1A0 Inverts the signal
-	TCCR1B |= /*(1 << WGM13) |*/ (1 << WGM12) /*| (1 << CS11)*/;
-
-	// Set the prescaler value to divide the clock by 2 (producing a 1 MHz timer clock)
-	TCCR1B |= (1 << CS10);
-
-	// Set the output compare register (OCR1A) to produce a 70% duty cycle
-	
-	usart_init();
 	char input[4];
 	uint16_t numeric_value = 0;
 	
+	pwm_init();
+	usart_init();
 
 	while (1)
 	{
@@ -44,10 +36,7 @@ int main(void)
 		numeric_value = ascii_to_numeric(input);
 		uart_tx(numeric_value);
 
-		//OCR1A = 1023;
-		OCR1A = numeric_value;
-
-		//OCR1A = data;
+		water_p1 = numeric_value;
 
 	}
 	return 0;
@@ -73,6 +62,19 @@ unsigned char uart_rx(void){
 	while (!(UCSRA & (1<<RXC)));            // bit mask to check if RXC bit is set (set = while (0), not set = while (1))
 	return UDR;                             // returns the received character from UDR register
 }
+
+void pwm_init(void){
+	// Set PD5 (OC1A) as output pin
+	DDRD |= (1 << PD5);
+
+	// Set Timer/Counter1 in Fast PWM mode, with non-inverted PWM output on OC1A
+	TCCR1A |= (1 << COM1A1) | (1 << COM1A0) | (1 << WGM11) | (1 << WGM10);		//COM1A0 Inverts the signal
+	TCCR1B |= /*(1 << WGM13) |*/ (1 << WGM12) /*| (1 << CS11)*/;
+
+	// Set the prescaler value to divide the clock by 2 (producing a 1 MHz timer clock)
+	TCCR1B |= (1 << CS10);
+}
+
 
 int ascii_to_numeric(char input[]){
 	uint16_t value = 0;

b02c54445e43b2b33114ffeeafa9535803f294e2
MaskedCamilo <camilo0599@gmail.com>
2023-03-15 12:36:04 -0400

    Update main.c

--- code/pwm/main.c
+++ code/pwm/main.c
@@ -1,6 +1,18 @@
-#define F_CPU 8000000UL
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <avr/io.h>
+#define F_CPU 8000000UL
 #include <util/delay.h>
+
+#define BAUDRATE 8			//57.6k bps at 8MHz
+
+void usart_init(void);
+void uart_tx(unsigned char);
+unsigned char uart_rx(void);
+int ascii_to_numeric(char input[]);
+
+int output[4];
 
 int main(void)
 {
@@ -16,13 +28,56 @@ int main(void)
 
 	// Set the output compare register (OCR1A) to produce a 70% duty cycle
 	
+	usart_init();
+	char input[4];
+	uint16_t numeric_value = 0;
+	
 
 	while (1)
 	{
-		OCR1A = 511;
-		//_delay_ms(5000);
-		//OCR1A = 0
-		//_delay_ms(5000);
+		// Wait for 4 characters input
+		for (int i = 0; i < 4; i++) {
+			input[i] = uart_rx();
+			uart_tx(input[i]);
+		}
+
+		numeric_value = ascii_to_numeric(input);
+		uart_tx(numeric_value);
+
+		//OCR1A = 1023;
+		OCR1A = numeric_value;
+
+		//OCR1A = data;
+
 	}
 	return 0;
+}
+
+//-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+//Initializes USART
+void usart_init (void){
+	UBRRL=BAUDRATE;								//sets baud rate
+	UCSRB|=(1<<TXEN)|(1<<RXEN);					//enables receiver and transmitter
+	UCSRC|=(1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);	// 8bit data format
+}
+
+//Transmits data to USART
+void uart_tx (unsigned char data){
+	while (!(UCSRA & (1<<UDRE)));           // bit mask to check if UDRE bit is set (set = while (0), not set = while (1))
+	UDR = data;                             // loads data to UDR register
+}
+
+unsigned char uart_rx(void){
+	while (!(UCSRA & (1<<RXC)));            // bit mask to check if RXC bit is set (set = while (0), not set = while (1))
+	return UDR;                             // returns the received character from UDR register
+}
+
+int ascii_to_numeric(char input[]){
+	uint16_t value = 0;
+	for (int i = 0; i < 4; i++) {
+		value = value * 10 + (input[i] - '0');
+	}
+	return value;
 }

3a2789835ea6377255c158f77452635bc8c944d7
MaskedCamilo <camilo0599@gmail.com>
2023-03-26 14:37:21 -0400

    v4.1

--- code/sensors/cap_sens/main.c
+++ code/sensors/cap_sens/main.c
@@ -5,22 +5,60 @@
 #include <util/delay.h>
 
 #define BAUDRATE 8
+
 #define cap_sen_t1_low PE0
+#define cap_sen_t1_high PE1
+#define cap_sen_t2_low PC3
+#define cap_sen_t2_high PC2
+#define cap_sen_pt_high PC1
 
 void usart_init(void);
 void usart_tx(unsigned char);
 
 int main (void)
 {
-	DDRE &= (1 << cap_sen_t1_low);
+	DDRE &= ~(1 << cap_sen_t1_low) & ~(1 << cap_sen_t1_high);
+	DDRC &= ~(1 << cap_sen_t2_low) & ~(1 << cap_sen_t2_high) & ~(1 << cap_sen_pt_high);
+	
 	usart_init();
 
 	int cap_sen_t1_low_val = 0;
+	int cap_sen_t1_high_val = 0;
+	int cap_sen_t2_low_val = 0;
+	int cap_sen_t2_high_val = 0;
+	int cap_sen_pt_high_val = 0;
 
 	while(1){
 		cap_sen_t1_low_val = (PINE & (1 << cap_sen_t1_low)) >> cap_sen_t1_low;
 		_delay_ms(100);
 		usart_tx(cap_sen_t1_low_val);
+		
+		_delay_ms(1000);
+		
+		cap_sen_t1_high_val = (PINE & (1 << cap_sen_t1_high)) >> cap_sen_t1_high;
+		_delay_ms(100);
+		usart_tx(cap_sen_t1_high_val);
+		
+		_delay_ms(1000);
+		
+		cap_sen_t2_low_val = (PINC & (1 << cap_sen_t2_low)) >> cap_sen_t2_low;
+		_delay_ms(100);
+		usart_tx(cap_sen_t2_low_val);
+		
+		_delay_ms(1000);
+		
+		cap_sen_t2_high_val = (PINC & (1 << cap_sen_t2_high)) >> cap_sen_t2_high;
+		_delay_ms(100);
+		usart_tx(cap_sen_t2_high_val);
+		
+		_delay_ms(1000);
+		
+		cap_sen_pt_high_val = (PINC & (1 << cap_sen_pt_high)) >> cap_sen_pt_high;
+		_delay_ms(100);
+		usart_tx(cap_sen_pt_high_val);
+		
+		_delay_ms(1000);
+		
 	}
 	return 0;
 }

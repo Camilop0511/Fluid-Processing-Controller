af711b8a9f4d8ff7afe47aef5c2f968801e9b324
MaskedCamilo <camilo0599@gmail.com>
2023-03-28 13:39:25 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -7,5 +7,20 @@
         <p>
             Liquid leve tank1: <?= (int)$_POST['tank1']; ?> 
         </p> 
+        <p>
+            Liquid leve tank2: <?= (int)$_POST['tank2']; ?> 
+        </p> 
+        <p>
+            Speed percentage water pump1: <?= (int)$_POST['water_p1']; ?> 
+        </p> 
+        <p>
+            Speed percentage water pump2: <?= (int)$_POST['water_p2']; ?> 
+        </p> 
+        <p>
+            Set temperature: <?= (int)$_POST['temp']; ?> 
+        </p> 
+        <p>
+            Cool-down time: <?= (int)$_POST['sec']; ?> 
+        </p> 
     </body>
 </html>

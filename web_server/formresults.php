80a304a0b4081434d4f0dba3a0f3105790c3ae37
MaskedCamilo <camilo0599@gmail.com>
2023-03-28 13:35:23 -0400

    Create formresults.php

---
+++ web_server/formresults.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>formresults.php</title>
+    </head>
+    <body> 
+        <p>
+            Liquid leve tank1: <?= (int)$_POST['tank1']; ?> 
+        </p> 
+    </body>
+</html>

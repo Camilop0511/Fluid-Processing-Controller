5e90950e0b7ef871926631ee909e3fc7b67cffdf
MaskedCamilo <camilo0599@gmail.com>
2023-04-01 17:36:11 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -9,6 +9,33 @@
    
    usleep(10000); // Wait for 10ms to ensure buffer is cleared
    
+   // Flush the input and output buffers
+   exec('stty -F /dev/ttyS0 -icrnl');
+   exec('stty -F /dev/ttyS0 -inlcr');
+   exec('stty -F /dev/ttyS0 -opost');
+   exec('stty -F /dev/ttyS0 -onlcr');
+   exec('stty -F /dev/ttyS0 -isig');
+   exec('stty -F /dev/ttyS0 -icanon');
+   exec('stty -F /dev/ttyS0 -echo');
+   exec('stty -F /dev/ttyS0 -echoe');
+   exec('stty -F /dev/ttyS0 -echok');
+   exec('stty -F /dev/ttyS0 -echoctl');
+   exec('stty -F /dev/ttyS0 -echoke');
+   exec('stty -F /dev/ttyS0 -crtscts');
+   exec('stty -F /dev/ttyS0 57600');
+   exec('stty -F /dev/ttyS0 -icrnl');
+   exec('stty -F /dev/ttyS0 -inlcr');
+   exec('stty -F /dev/ttyS0 -opost');
+   exec('stty -F /dev/ttyS0 -onlcr');
+   exec('stty -F /dev/ttyS0 -isig');
+   exec('stty -F /dev/ttyS0 -icanon');
+   exec('stty -F /dev/ttyS0 -echo');
+   exec('stty -F /dev/ttyS0 -echoe');
+   exec('stty -F /dev/ttyS0 -echok');
+   exec('stty -F /dev/ttyS0 -echoctl');
+   exec('stty -F /dev/ttyS0 -echoke');
+   exec('stty -F /dev/ttyS0 -crtscts');
+   
    $value = 67;
    fwrite($serialPort, chr($value));
    usleep(1000);

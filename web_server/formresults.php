e6c584829083d5bfca9a7cae4df629c11921226b
MaskedCamilo <camilo0599@gmail.com>
2023-04-01 17:57:17 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -2,7 +2,7 @@
    $serialPort = fopen('/dev/ttyS0', 'w');
    system('stty -F /dev/ttyS0 57600');
    
-   // Clear any existing data in the buffer
+   /*// Clear any existing data in the buffer
    stream_set_blocking($serialPort, false);
    stream_set_timeout($serialPort, 0);
    $clearData = fread($serialPort, 8192);
@@ -34,7 +34,7 @@
    exec('stty -F /dev/ttyS0 -echok');
    exec('stty -F /dev/ttyS0 -echoctl');
    exec('stty -F /dev/ttyS0 -echoke');
-   exec('stty -F /dev/ttyS0 -crtscts');
+   exec('stty -F /dev/ttyS0 -crtscts');*/
    
    $value = $_POST['tank1']; // Obtain the value of tank1 using POST
    fwrite($serialPort, chr($value));

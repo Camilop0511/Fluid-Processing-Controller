f00258b98d60f7449b7ec75456fdf1a45adc94f6
MaskedCamilo <camilo0599@gmail.com>
2023-04-01 16:42:08 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -1,40 +1,42 @@
+<?php
+$values = array(
+    'tank1' => (int)$_POST['tank1'],
+    'tank2' => (int)$_POST['tank2'],
+    'water_p1' => (int)$_POST['water_p1'],
+    'water_p2' => (int)$_POST['water_p2'],
+    'temp' => (int)$_POST['temp'],
+    'sec' => (int)$_POST['sec']
+);
+
+// Convert values to hex format
+$hexValues = array_map('dechex', $values);
+
+// Build command to send data over serial with screen
+$serialDevice = '/dev/ttyS0'; // Replace with your device name
+$baudRate = 57600;
+$parity = 'n';
+$dataBits = 8;
+$stopBits = 1;
+$command = "screen -S serial -dm bash -c 'stty -F $serialDevice $baudRate cs$dataBits $parity stopbits $stopBits && echo -n " . implode('', $hexValues) . " > $serialDevice'";
+
+// Execute command in shell
+shell_exec($command);
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>formresults.php</title>
-    </head>
-    <body> 
-        <?php
-            $values = array(
-                'tank1' => (int)$_POST['tank1'],
-                'tank2' => (int)$_POST['tank2'],
-                'water_p1' => (int)$_POST['water_p1'],
-                'water_p2' => (int)$_POST['water_p2'],
-                'temp' => (int)$_POST['temp'],
-                'sec' => (int)$_POST['sec']
-            );
-            $port = fopen("/dev/ttyUSB0", "w");
-            fwrite($port, "Starting data transmission\n");
-            foreach ($values as $key => $value) {
-                $hexValue = dechex($value);
-                $bytes = str_split($hexValue, 2);
-
-                foreach ($bytes as $byte) {
-                    $byteValue = hexdec($byte);
-                    fwrite($port, chr($byteValue));
-                    usleep(1000); // wait for 1 millisecond
-                }
-            }
-            fwrite($port, "Data transmission complete\n");
-            fclose($port);
-        ?>
-        <p>Data sent over serial in hex format:</p>
-        <ul>
-            <?php foreach ($values as $key => $value) : ?>
-                <li><?= $key ?>: <?= dechex($value) ?></li>
-            <?php endforeach; ?>
-        </ul>
-    </body>
+<head>
+    <title>formresults.php</title>
+</head>
+<body>
+    <p>Data sent over serial in hex format:</p>
+    <ul>
+        <?php foreach ($hexValues as $key => $value) : ?>
+            <li><?= $key ?>: <?= $value ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
 </html>
+
 
 

dc423458788605c8541278363a64d4ac26e9cfd9
MaskedCamilo <camilo0599@gmail.com>
2023-04-01 16:40:50 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -1,31 +1,40 @@
-<?php
-$values = array(
-    'tank1' => (int)$_POST['tank1'],
-    'tank2' => (int)$_POST['tank2'],
-    'water_p1' => (int)$_POST['water_p1'],
-    'water_p2' => (int)$_POST['water_p2'],
-    'temp' => (int)$_POST['temp'],
-    'sec' => (int)$_POST['sec']
-);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>formresults.php</title>
+    </head>
+    <body> 
+        <?php
+            $values = array(
+                'tank1' => (int)$_POST['tank1'],
+                'tank2' => (int)$_POST['tank2'],
+                'water_p1' => (int)$_POST['water_p1'],
+                'water_p2' => (int)$_POST['water_p2'],
+                'temp' => (int)$_POST['temp'],
+                'sec' => (int)$_POST['sec']
+            );
+            $port = fopen("/dev/ttyUSB0", "w");
+            fwrite($port, "Starting data transmission\n");
+            foreach ($values as $key => $value) {
+                $hexValue = dechex($value);
+                $bytes = str_split($hexValue, 2);
 
-$serial_port = "/dev/ttyS0";
-$baud_rate = 57600;
-$command = "screen -L $serial_port $baud_rate,cs8,-parenb,-cstopb";
+                foreach ($bytes as $byte) {
+                    $byteValue = hexdec($byte);
+                    fwrite($port, chr($byteValue));
+                    usleep(1000); // wait for 1 millisecond
+                }
+            }
+            fwrite($port, "Data transmission complete\n");
+            fclose($port);
+        ?>
+        <p>Data sent over serial in hex format:</p>
+        <ul>
+            <?php foreach ($values as $key => $value) : ?>
+                <li><?= $key ?>: <?= dechex($value) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
 
-foreach ($values as $key => $value) {
-    $hexValue = dechex($value);
-    $bytes = str_split($hexValue, 2);
-
-    foreach ($bytes as $byte) {
-        $byteValue = hexdec($byte);
-        $escapedByte = escapeshellarg(chr($byteValue));
-        $cmd = "echo -ne $escapedByte > $serial_port";
-        shell_exec($cmd);
-        usleep(1000); // wait for 1 millisecond
-    }
-}
-
-// Open screen and send data
-shell_exec($command);
-?>
 

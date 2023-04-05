93ab1e5b5c585a596e58639f5c1750fb0ec97b15
MaskedCamilo <camilo0599@gmail.com>
2023-04-05 10:48:39 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -26,10 +26,10 @@
 </html>
 
 <?php
-   /*$serialPort = fopen('/dev/ttyS0', 'w');
+   $serialPort = fopen('/dev/ttyS0', 'w');
    system('stty -F /dev/ttyS0 57600');
    
-   // Clear any existing data in the buffer
+   /*// Clear any existing data in the buffer
    stream_set_blocking($serialPort, false);
    stream_set_timeout($serialPort, 0);
    $clearData = fread($serialPort, 8192);

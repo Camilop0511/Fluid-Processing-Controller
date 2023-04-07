c6b02f3bec2e31a9bc2654a5c08d4d4b791e28b2
MaskedCamilo <camilo0599@gmail.com>
2023-04-07 16:47:47 -0400

    test

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -133,12 +133,12 @@
    fwrite($serialPort, $cooldown);
    usleep(5000);
 
-   $byte1 = 10; // Put your fixed byte value here
+   /*$byte1 = 10; // Put your fixed byte value here
    $byte2 = 1; // Receive the tank2 value through POST
    $start = array($byte1, $byte2); // Create an array with two bytes
    $start = implode(array_map("chr", $start)); // Convert array to string of characters
    fwrite($serialPort, $start);
-   usleep(5000);
+   usleep(5000);*/
 
    fclose($serialPort);
 ?>

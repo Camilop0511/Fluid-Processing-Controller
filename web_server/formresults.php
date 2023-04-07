1feed708d60ab216249f4051e48f6112b2125300
MaskedCamilo <camilo0599@gmail.com>
2023-04-07 16:08:43 -0400

    Update formresults.php

--- web_server/formresults.php
+++ web_server/formresults.php
@@ -1,11 +1,17 @@
 <?php
 if(isset($_POST['start'])) {
+
+    $serialPort = fopen('/dev/ttyS0', 'w');
+    system('stty -F /dev/ttyS0 57600');
+
    $byte1 = 10; // Put your fixed byte value here
    $byte2 = 1; // Receive the tank2 value through POST
    $start = array($byte1, $byte2); // Create an array with two bytes
    $start = implode(array_map("chr", $start)); // Convert array to string of characters
    fwrite($serialPort, $start);
    usleep(5000);
+
+   fclose($serialPort);
 }
 
 if(isset($_POST['stop'])) {

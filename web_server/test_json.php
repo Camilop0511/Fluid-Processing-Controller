a72d7f105b1d4aaf7b1a55872265c1794bd92f4a
MaskedCamilo <camilo0599@gmail.com>
2023-04-30 12:13:46 -0400

    JSON test

---
+++ web_server/test_json.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			setInterval(function() {
+				// make an AJAX request to fetch the values from serial_read.php
+				$.getJSON('test_json2', function(data) {
+					// update the HTML content with the real_volume value
+					$("#real-volume").html(data.real_volume);
+				});
+			}, 5000); // fetch the data every 5 seconds
+		});
+	</script>
+</head>
+<body>
+	<p>The real volume is: <span id="real-volume"></span></p>
+</body>
+</html>
---
+++ web_server/test_json2.php
@@ -0,0 +1,8 @@
+<?php
+    $real_volume = 100;
+    
+	$data = array(
+    'real_volume' => $real_volume);
+	
+    echo json_encode($data);
+?>
